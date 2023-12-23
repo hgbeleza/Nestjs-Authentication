be8f1a5065c270b8d45bb83064a58577e43b7c86
hgbeleza <developer.beleza06@gmail.com>
2023-12-23 14:20:11 -0300

    feat(hgbeleza): prisma.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [
-    AppService, 
+    AppService,
+    PrismaService, 
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+      super();
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+        this.enableShutdownHooks();
+    }
+
+    enableShutdownHooks() {
+        process.on('beforeExit', async () => {
+            await this.$disconnect();
+        });
+    }
+}
