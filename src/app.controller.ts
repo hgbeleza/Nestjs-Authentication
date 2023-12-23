bd9542ea1f7041014e609660bcf260c78b93adbd
hgbeleza <developer.beleza06@gmail.com>
2023-12-22 21:45:02 -0300

    add project

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from './auth/auth.guard';
+import { Public } from './auth/is-public';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Public()
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
