2c66ac4c6d30867ce9b781ddc27270aaf4a06a40
hgbeleza <developer.beleza06@gmail.com>
2023-12-23 22:46:16 -0300

    feat(hgbeleza): bcrypt & cheking email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,39 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const checkEmail = await this.prisma.user.findUnique({
+      where: {
+        email: createUserDto.email
+      }
+    });
+
+    if(checkEmail) {
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'Email already exists',
+      }, HttpStatus.FORBIDDEN);
+    }
+
     const data: Prisma.UserCreateInput = {
-      ...createUserDto
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 12)
     }
 
     const createdUser = await this.prisma.user.create({data});
 
-    return createdUser;
+    return {
+      ...createdUser,
+      password: undefined
+    };
   }
 
   async findByEmail(email: string): Promise<User> {
